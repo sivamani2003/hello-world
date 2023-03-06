0f95102b1279d0c9c97b579aec2da21e25c88fb2
siva <sivamani_k@srmap.edu.in>
2023-03-06 15:07:31 +0530

    update

---
+++ src/component/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Greet = props => {
+    console.log(props)
+    return (
+        <div>
+            <h1>
+                hello {props.name} {props.heroname}
+            </h1>
+            {props.children}
+        </div>
+    )
+    }    
+export default Greet 
---
+++ src/component/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Hello = () => {
+    return(
+        <div className="siva"> 
+            <h1>hello everyone</h1>
+        </div>
+    )
+}
+export default Hello
